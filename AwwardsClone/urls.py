e53f94030eba828d9d408bd843fd621c7a2f15d1
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-06-11 15:21:50 +0300

    Add and connect the static files and templates

--- AwwardsClone/urls.py
+++ AwwardsClone/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accolades.views import landingPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landingPage, name="landingPage"),
 ]
--- accolades/views.py
+++ accolades/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landingPage(request):
+  '''
+  View function that renders the landing page and its information
+  '''
+  return render(request, "landingPage.html", {})
