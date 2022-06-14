1fc85854519252c5c86eb2b1f45f88739aa9a684
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-06-14 10:55:29 +0300

    Add styling to the rating section

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,28 @@ $(document).ready(function(){
   })
 
 })
+
+const allStars = document.querySelectorAll('.d-star');
+let current_rating = document.querySelector('.current_rating')
+
+
+allStars.forEach( (star, i) =>{
+  star.onclick = function(){
+    // Current Level of clicked star
+    let current_star_level = i + 1;
+    // current_rating.innerText = `${current_star_level} of 10`;
+    current_rating.innerHTML = current_star_level + " of 10";
+    current_rating.style.fontWeight = "bold";
+
+    allStars.forEach( (star, j) => {
+      // Color the stars
+      if(current_star_level >= j + 1){
+        star.innerHTML = '&#9733'
+      }
+      // Discolor the stars
+      else{
+        star.innerHTML = '&#9734'
+      }
+    })
+  }
+})
