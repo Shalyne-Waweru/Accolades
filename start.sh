6ede079fdb46760b371d1b0e9ccb1df3f09ae01f
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-06-14 12:00:33 +0300

    heroku deployment

--- start.sh
+++ start.sh
@@ -2,14 +2,14 @@
 # heroku config:set API_SECRET=Jvd78nFKUziejHAW-b5Qn0gLuHk
 # heroku config:set CLOUDINARY_URL=cloudinary://123334979696849:Jvd78nFKUziejHAW-b5Qn0gLuHk@da7srpwm6
 # heroku config:set CLOUD_NAME=da7srpwm6
-heroku config:set DEBUG=False
-heroku config:set DISABLE_COLLECTSTATIC=1
-heroku config:set MODE=prod
-heroku config:set SECRET_KEY='django-insecure-h-4_vq % @6x462t8ly = k = +8os_54n7_lziad!i4 *$_rey9b@1mb'
-heroku config:set ALLOWED_HOSTS=project-accolades.herokuapp.com
+# heroku config:set DEBUG=False
+# heroku config:set DISABLE_COLLECTSTATIC=1
+# heroku config:set MODE=prod
+# heroku config:set SECRET_KEY='django-insecure-h-4_vq % @6x462t8ly = k = +8os_54n7_lziad!i4 *$_rey9b@1mb'
+# heroku config:set ALLOWED_HOSTS=project-accolades.herokuapp.com
 
 git switch master
-python manage.py collectstatic
+# python manage.py collectstatic
 pip freeze > requirements.txt
 git add .
 git commit -m "heroku deployment"
